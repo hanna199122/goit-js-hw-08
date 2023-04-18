bd7720b9f55b825c917e75b0da6a2c3d0e86e715
Hanna <malahovaa79@gmail.com>
2023-04-18 23:24:01 +0300

    after checking by mentor

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -28,6 +28,14 @@ function populateTextarea() {
   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
   if (savedMessage) {
-    refs.textarea.value = savedMessage;
+    const { email, message } = savedMessage;
+
+    refs.form.email.value = email;
+    refs.form.message.value = message;
+
+    console.log(refs.form.email.value);
+    console.log(refs.form.message.value);
   }
 }
+
+// Вітаю! Гарно попрацювали, залишилось доопрацювати над 3 завданням,а саме ви зберігаєте об'єкт formData в локальне сховище, а потім спробували встановити цей об'єкт як значення для refs.textarea.value.У рядку refs.textarea.value = savedMessage;, savedMessage містить розпарсений об'єкт formData, а не значення поля message, таким чином при перезагрузці сторінки у вас повертається у текстерії [object Object], щоб вирішити цю проблему, вам потрібно встановити refs.textarea.value на значення поля message збереженого об'єкта savedMessage .Чекаю вашу доопрацьовану роботу)
